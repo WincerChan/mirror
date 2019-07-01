43868eb20286d098e149a81173bc8e069272249a
WincerChan <WincerChan@gmail.com>
2019-07-01 17:54:03 +0800

    :construction: Add struct for config
    :construction: Add struct for config(2)
    
    :construction: Add struct for config(3)

---
+++ tool.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func loadConfig() {
+	Config = new(Yaml)
+	yamlFile, err := ioutil.ReadFile("config.yaml")
+	checkErr(err)
+	err = yaml.Unmarshal(yamlFile, Config)
+	checkErr(err)
+	if Config.EnableSSL {
+		protocal = "https://"
+	} else {
+		protocal = "http://"
+	}
+	log.Println("conf", Config.ReplacedStrings)
+}
