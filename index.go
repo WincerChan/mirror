de07e7d5d9cca6793c16225773e5eeb3963b74f8
WincerChan <WincerChan@gmail.com>
2019-07-02 00:53:10 +0800

    :hammer: Remove other go files

--- index.go
+++ index.go
@@ -1,26 +1,83 @@
-package main
+package mirror
 
 import (
 	"bytes"
 	"compress/gzip"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"regexp"
 	"strconv"
 	"strings"
+
+	"gopkg.in/yaml.v2"
 )
-
-// var host = "s2-us2.startpage.com"
-
-// var Config.Host.Self = "mirror.loerfy.now.sh"
 
 var Config *Yaml
 var initial bool
 var protocal string
 
+type Replaced struct {
+	Old string `yaml:"old"`
+	New string `yaml:"new"`
+}
+
+type Yaml struct {
+	Host struct {
+		Self  string `yaml:"self"`
+		Proxy string `yaml:"proxy"`
+	}
+	ReplacedURLs []Replaced `yaml:"replaced_urls"`
+	EnableSSL    bool       `yaml:"enable_ssl"`
+	HandleCookie bool       `yaml:"handle_cookie"`
+}
+
+var data = `
+enable_ssl: True
+handle_cookie: True
+
+host:
+  self: mirror.loerfy.now.sh
+  proxy: s2-us2.startpage.com
+
+replaced_urls:
+  - old: www.startpage.com
+    new: s2-us2.startpage.com
+  - old: s2-us2.startpage.com
+    new: mirror.loerfy.now.sh
+`
+
+func loadConfig() {
+	Config = new(Yaml)
+	err := yaml.Unmarshal([]byte(data), Config)
+	checkErr(err)
+	if Config.EnableSSL {
+		protocal = "https://"
+	} else {
+		protocal = "http://"
+	}
+}
+
+func configStruct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h3><a href='/bye/bye'>Leave</a>")
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func hasGziped(coding string) bool {
+	return strings.HasPrefix(coding, "gz")
+}
+
+func isTextType(typeName string) bool {
+	return strings.HasPrefix(typeName, "text") ||
+		strings.HasPrefix(typeName, "appli")
+}
 func replaceText(text []byte) []byte {
 	for _, url := range Config.ReplacedURLs {
 		text = bytes.ReplaceAll(text,
@@ -99,9 +156,9 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	proxy.ServeHTTP(w, r)
 }
 
-func main() {
-	http.HandleFunc("/", Handler)
-	// http.ListenAndServe(":3000", nil)
-	http.ListenAndServeTLS(":3000", "/home/wincer/.local/share/mkcert/rootCA.pem", "/home/wincer/.local/share/mkcert/rootCA-key.pem", nil)
-	log.Println("Listening in :3000")
-}
+// func main() {
+// 	http.HandleFunc("/", Handler)
+// 	// http.ListenAndServe(":3000", nil)
+// 	http.ListenAndServeTLS(":3000", "/home/wincer/.local/share/mkcert/rootCA.pem", "/home/wincer/.local/share/mkcert/rootCA-key.pem", nil)
+// 	log.Println("Listening in :3000")
+// }
