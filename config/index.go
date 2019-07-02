c67069938d5b48716e7de3895de84f4006d39d02
WincerChan <WincerChan@gmail.com>
2019-07-02 14:34:57 +0800

    :wrench: Remove config data

--- config/index.go
+++ config/index.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"io/ioutil"
 	T "mirror/tool"
 
 	"gopkg.in/yaml.v2"
@@ -21,26 +22,14 @@ type Yaml struct {
 	HandleCookie bool       `yaml:"handle_cookie"`
 }
 
-var data = `
-enable_ssl: True
-handle_cookie: True
-
-host:
-  self: mirror.loerfy.now.sh
-  proxy: s2-us2.startpage.com
-
-replaced_urls:
-  - old: www.startpage.com
-    new: s2-us2.startpage.com
-  - old: s2-us2.startpage.com
-    new: mirror.loerfy.now.sh
-`
 var Config *Yaml
 var Protocal string
 
 func LoadConfig() {
 	Config = new(Yaml)
-	err := yaml.Unmarshal([]byte(data), Config)
+	yamlFile, err := ioutil.ReadFile("./config.yaml")
+	T.CheckErr(err)
+	err = yaml.Unmarshal(yamlFile, Config)
 	T.CheckErr(err)
 	if Config.EnableSSL {
 		Protocal = "https://"
